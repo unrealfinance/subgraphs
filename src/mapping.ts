536eddb311659963e2b99c930e91df8be940074d
aniket965 <aniket965.as@gmail.com>
2021-05-03 20:14:56 +0530

    feat: working

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, log } from "@graphprotocol/graph-ts"
 import {
   Core,
   FutureStarted,
@@ -10,18 +10,18 @@ import {
 import {User,Stream,Epoch } from "../generated/schema"
 
 export function handleFutureStarted(event: FutureStarted): void {
-  let epoch = Epoch.load(event.params.metadata.toHex());
-  let stream = Stream.load(event.params.metadata.toHex())
+  // let epoch = Epoch.load(event.params.metadata.toHex());
+  // let stream = Stream.load(event.params.metadata.toHex())
   
-  if (epoch == null) epoch = new Epoch(event.params.metadata.toHex())
+  // if (epoch == null) epoch = new Epoch(event.params.metadata.toHex())
 
-  epoch.stream = stream.id
-  epoch.number = event.params.futureIndex
-  epoch.save()
+  // epoch.stream = stream.id
+  // epoch.number = event.params.futureIndex
+  // epoch.save()
   
-  if (stream !== null) stream.epochs = stream.epochs.concat([event.params.metadata.toHex()])
+  // if (stream !== null) stream.epochs = stream.epochs.concat([event.params.metadata.toHex()])
   
-  stream.save()
+  // stream.save()
   
 }
 
@@ -31,46 +31,46 @@ export function handleNewStreamCreated(event: NewStreamCreated): void {
     stream = new Stream(event.params.metadata.toHex())
     stream.meta = event.params.metadata
     stream.users = []
-    stream.epochs = []
   }
   stream.meta = event.params.metadata
   stream.save();
 }
 
 export function handleNewSubscription(event: NewSubscription): void {
-  let stream = Stream.load(event.params.metadata.toHex())
-  let user = User.load(event.params.user.toHex())
-  if (user == null) {
-    user = new User(event.params.user.toHex())
-    user.address = event.params.user
-    user.streams = []
-  }
-  user.save()
+  // log.info('hi',[])
 
-  if(stream !== null) {
-    user.streams = user.streams.concat([event.params.metadata.toHex()])
-    stream.users = stream.users.concat([event.params.user.toHex()])
-    user.save()
-    stream.save()
-  }
+  // let stream = Stream.load(event.params.metadata.toHex())
+  // let user = User.load(event.params.user.toHex())
+  // if (user == null) {
+  //   user = new User(event.params.user.toHex())
+  //   user.address = event.params.user
+  //   user.streams = []
+  // }
+  // user.save()
+
+  // if(stream !== null) {
+  //   user.streams = user.streams.concat([event.params.metadata.toHex()])
+  //   stream.users = stream.users.concat([event.params.user.toHex()])
+  //   user.save()
+  //   stream.save()
+  // }
 }
 
-export function handlePrincipalRedeemed(event: PrincipalRedeemed): void {}
 
 export function handleUnsubscribed(event: Unsubscribed): void {
-  let stream = Stream.load(event.params.metadata.toHex())
-  let user = User.load(event.params.user.toHex())
-  if (user == null) {
-    user = new User(event.params.user.toHex())
-    user.address = event.params.user
-    user.streams = []
-  }
-  user.save()
+  // let stream = Stream.load(event.params.metadata.toHex())
+  // let user = User.load(event.params.user.toHex())
+  // if (user == null) {
+  //   user = new User(event.params.user.toHex())
+  //   user.address = event.params.user
+  //   user.streams = []
+  // }
+  // user.save()
 
-  if(stream !== null) {
-    user.streams = user.streams.filter(ustream => ustream !== event.params.metadata.toHex())
-    stream.users = stream.users.filter(suser => suser !== event.params.user.toHex())
-    user.save()
-    stream.save()
-  }
+  // if(stream !== null) {
+  //   user.streams = user.streams.filter(ustream => ustream !== event.params.metadata.toHex())
+  //   stream.users = stream.users.filter(suser => suser !== event.params.user.toHex())
+  //   user.save()
+  //   stream.save()
+  // }
 }
