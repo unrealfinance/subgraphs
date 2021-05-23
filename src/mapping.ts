1f224726e7cdf5e90551a5fb821ef84eaf675f5d
aniket965 <aniket965.as@gmail.com>
2021-05-23 18:47:13 +0530

    feat: tokens

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,21 +1,42 @@
-import { BigInt, log } from "@graphprotocol/graph-ts"
+import { BigInt ,log,Address} from "@graphprotocol/graph-ts"
 import {
+  
   Core,
   EpochStarted,
   NewStream,
   Subscribed,
   Unsubscribed
 } from "../generated/Core/Core"
-import {User,Stream,Epoch } from "../generated/schema"
-
+import {ERC20} from '../generated/Core/ERC20'
+import {User,Stream,Epoch,OTTOKEN, YieldToken } from "../generated/schema"
+const coreAddress:Address =  Address.fromString('0xbf5292d67eC2752bAb725F63e2f571fdC64D2997');
 export function handleEpochStarted(event: EpochStarted): void {
+
   let epoch = Epoch.load(event.params.streamKey.toHex());
   let stream = Stream.load(event.params.streamKey.toHex())
-  
+
   if (epoch == null) epoch = new Epoch(event.params.streamKey.toHex())
 
   epoch.stream = stream.id
   epoch.number = event.params.futureIndex + BigInt.fromString('1')
+  
+  let core = Core.bind(coreAddress);
+  
+  let ytAddress = core.getYT(event.params.streamKey,event.params.futureIndex);
+
+  let contract = ERC20.bind(ytAddress)
+  let symbol = contract.symbol()
+  let name = contract.name();
+
+  let yieldToken = YieldToken.load(ytAddress.toHexString())
+  if (yieldToken == null) {
+    yieldToken = new YieldToken(ytAddress.toHexString())
+    yieldToken.symbol = symbol
+    yieldToken.name = name
+  }
+  yieldToken.save()
+
+  
   epoch.save()
   
   if (stream !== null) stream.epochs = stream.epochs.concat([event.params.streamKey.toHex()])
@@ -31,11 +52,27 @@ export function handleNewStream(event: NewStream): void {
     stream.meta = event.params.streamKey
     stream.users = []
     stream.epochs = []
+
+  let core = Core.bind(coreAddress);
+  
+  let OTaddress = core.getOT(event.params.streamKey);
+
+  let contract = ERC20.bind(OTaddress)
+  let symbol = contract.symbol()
+  let name = contract.name();
+
+  let otToken = OTTOKEN.load(OTaddress.toHexString())
+  if (otToken == null) {
+    otToken = new OTTOKEN(OTaddress.toHexString())
+    otToken.symbol = symbol
+    otToken.name = name
   }
+  otToken.save()
+
   stream.meta = event.params.streamKey
   stream.save();
 }
-
+}
 export function handleSubscribed(event: Subscribed): void {
 
   let stream = Stream.load(event.params.streamKey.toHex())
