ece10effb8c4c32aca39029adcf1411ef1a56096
aniket965 <aniket965.as@gmail.com>
2021-06-02 20:01:48 +0530

    fixes: for new contract

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt, Address } from '@graphprotocol/graph-ts'
+import { BigInt, Address, log } from '@graphprotocol/graph-ts'
 import {
   Core,
   EpochStarted,
@@ -9,18 +9,30 @@ import {
 import { ERC20 } from '../generated/Core/ERC20'
 import { User, Stream, Epoch, OTTOKEN, YieldToken, TokenSubscription } from '../generated/schema'
 const coreAddress: Address = Address.fromString(
-  '0x57c9e8D17bE3f506c6527e27362B905842E0340e',
+  '0xcFaE8aC2A6a87F7194b0cB0eC9C53811F6350126',
 )
 export function handleEpochStarted(event: EpochStarted): void {
-  let epoch = Epoch.load(event.params.streamKey.toHex()+ "-" + event.params.futureIndex.toHexString())
+  let epoch = Epoch.load(event.params.streamKey.toHex() + "-" + event.params.futureIndex.toHexString())
   let stream = Stream.load(event.params.streamKey.toHex())
 
-  if (epoch == null) epoch = new Epoch(event.params.streamKey.toHex()+ "-" + event.params.futureIndex.toHexString())
+  if (epoch == null) epoch = new Epoch(event.params.streamKey.toHex() + "-" + event.params.futureIndex.toHexString())
   epoch.subscriptions = []
   epoch.stream = stream.id
   epoch.number = event.params.futureIndex
 
   let core = Core.bind(coreAddress)
+  let OTaddress = core.getOT(event.params.streamKey)
+  let contractOt = ERC20.bind(OTaddress)
+
+  let otToken = OTTOKEN.load(OTaddress.toHexString())
+
+  if (otToken == null) {
+    otToken = new OTTOKEN(OTaddress.toHexString())
+    otToken.symbol = contractOt.symbol()
+    otToken.name = contractOt.name()
+    otToken.address = OTaddress.toHexString()
+  }
+  otToken.save()
 
   let ytAddress = core.getYT(event.params.streamKey, event.params.futureIndex)
 
@@ -40,10 +52,9 @@ export function handleEpochStarted(event: EpochStarted): void {
   epoch.save()
 
   if (stream !== null)
-    stream.epochs = stream.epochs.concat([event.params.streamKey.toHex()+ "-" + event.params.futureIndex.toHexString()])
+    stream.epochs = stream.epochs.concat([event.params.streamKey.toHex() + "-" + event.params.futureIndex.toHexString()])
 
-
-
+  stream.otToken = OTaddress.toHexString()
   stream.save()
 }
 
@@ -57,40 +68,24 @@ export function handleNewStream(event: NewStream): void {
     stream.protocol = event.params.protocol
     stream.underlying = event.params.underlying.toHexString()
     stream.durationBlocks = event.params.durationBlocks
-    let core = Core.bind(coreAddress)
 
-    let OTaddress = core.getOT(event.params.streamKey)
-
-    let contract = ERC20.bind(OTaddress)
-    let symbol = contract.symbol()
-    let name = contract.name()
-
-    let otToken = OTTOKEN.load(OTaddress.toHexString())
-    if (otToken == null) {
-      otToken = new OTTOKEN(OTaddress.toHexString())
-      otToken.symbol = symbol
-      otToken.name = name
-      otToken.address = OTaddress.toHexString()
-    }
-    otToken.save()
-    stream.otToken = OTaddress.toHexString()
     stream.meta = event.params.streamKey
     stream.save()
   }
 }
 export function handleSubscribed(event: Subscribed): void {
   let subscription = TokenSubscription.load(event.params.streamKey.toHex() + event.params.user.toHex())
-  if(subscription == null) {
+  if (subscription == null) {
     subscription = new TokenSubscription(event.params.streamKey.toHex() + event.params.user.toHex())
     subscription.user = event.params.user.toHex()
-    subscription.stream  = event.params.streamKey.toHex()
+    subscription.stream = event.params.streamKey.toHex()
     subscription.amount = event.params.amount;
   } else {
-    subscription.amount =  subscription.amount.plus(event.params.amount);  
+    subscription.amount = subscription.amount.plus(event.params.amount);
   }
 
   subscription.save()
-  
+
   let stream = Stream.load(event.params.streamKey.toHex())
   let user = User.load(event.params.user.toHex())
 
@@ -110,9 +105,9 @@ export function handleSubscribed(event: Subscribed): void {
     user.streams = user.streams.concat([event.params.streamKey.toHex()])
     stream.users = stream.users.concat([event.params.user.toHex()])
     let epochs = stream.epochs;
-    let epochid:string = epochs.reverse()[0];
-    let epoch  = Epoch.load(epochid)
-    if(epoch !== null) {
+    let epochid: string = epochs.reverse()[0];
+    let epoch = Epoch.load(epochid)
+    if (epoch !== null) {
       epoch.subscriptions = epoch.subscriptions.concat([event.params.streamKey.toHex() + event.params.user.toHex()])
     }
     epoch.save()
@@ -125,8 +120,8 @@ export function handleSubscribed(event: Subscribed): void {
 
 export function handleUnsubscribed(event: Unsubscribed): void {
   let subscription = TokenSubscription.load(event.params.streamKey.toHex() + event.params.user.toHex())
-  if(subscription !== null)
-    subscription.amount =  subscription.amount.minus(event.params.amount);  
+  if (subscription !== null)
+    subscription.amount = subscription.amount.minus(event.params.amount);
 
   subscription.save()
   let stream = Stream.load(event.params.streamKey.toHex())
